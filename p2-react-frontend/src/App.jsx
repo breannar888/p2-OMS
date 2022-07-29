adc259b8f1f21f1ea790b0ce17fe412838738b68
breannar888 <67029835+breannar888@users.noreply.github.com>
2022-07-29 14:20:34 -0500

    populate fields with order data

--- p2-react-frontend/src/App.jsx
+++ p2-react-frontend/src/App.jsx
@@ -1,27 +1,35 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
-import { AddForm, NavSidebar, OrdersLog, OrdersTable, Menu } from "./components/";
-import { AddOrderPage } from "./pages/AddOrderPage";
+import {
+  AddForm,
+  NavSidebar,
+  OrdersLog,
+  OrdersTable,
+  Menu,
+} from "./components/";
+import OrderProvider from "./context/OrderContext";
 
 function App() {
   return (
     <div className="row">
-      <BrowserRouter>
-        <NavSidebar>
-          <Link to="/add"> Add Orders</Link>
-          <Link to="/current"> Current Orders</Link>
-          <Link to="/menu"> Manage Menu</Link>
-          <Link to="/log"> Orders Log</Link>
-        </NavSidebar>
-        <Routes>
-          <Route path="/" element={<AddForm />}></Route>
-          <Route path="/add" element={<AddForm />}></Route>
-          <Route path="/current" element={<OrdersTable />}></Route>
-          <Route path="/menu" element={<Menu />}></Route>
-          <Route path="/log" element={<OrdersLog />}></Route>
-          <Route path="/*" element={<AddForm />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <OrderProvider>
+        <BrowserRouter>
+          <NavSidebar>
+            <Link to="/add"> Add Orders</Link>
+            <Link to="/current"> Current Orders</Link>
+            <Link to="/menu"> Manage Menu</Link>
+            <Link to="/log"> Orders Log</Link>
+          </NavSidebar>
+          <Routes>
+            <Route path="/" element={<AddForm />}></Route>
+            <Route path="/add" element={<AddForm />}></Route>
+            <Route path="/current" element={<OrdersTable />}></Route>
+            <Route path="/menu" element={<Menu />}></Route>
+            <Route path="/log" element={<OrdersLog />}></Route>
+            <Route path="/*" element={<AddForm />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </OrderProvider>
     </div>
   );
 }
--- p2-react-frontend/src/components/OrdersTable.jsx
+++ p2-react-frontend/src/components/OrdersTable.jsx
@@ -1,119 +1,110 @@
-import React from 'react'
+import React from "react";
+import { OrderState } from "../context/OrderContext";
 
 export const OrdersTable = () => {
+  const { order, setOrder } = OrderState();
+
   return (
-    <main class="container col-9">
-        <h1>Current Orders</h1>
-        <div class="row container">
-            <div class="col ">
-                <h2>New Orders</h2>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Ticket</th>
-                            <th scope="col">Item</th>
-                            <th scope="col">Notes</th>
-                            <th scope="col">Edit</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Steak</td>
-                            <td>Well Done</td>
-                            <td>
-                                <i class="material-symbols-outlined trash">delete_forever</i>
-                                <i class="material-symbols-outlined next">east</i>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Eggs</td>
-                            <td>Sunny Side</td>
-                            <td>
-                                <i class="material-symbols-outlined trash">delete_forever</i>
-                                <i class="material-symbols-outlined next">east</i>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">4</th>
-                            <td>Ham</td>
-                            <td></td>
-                            <td>
-                                <i class="material-symbols-outlined trash">delete_forever</i>
-                                <i class="material-symbols-outlined next">east</i>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">4</th>
-                            <td>Eggs</td>
-                            <td></td>
-                            <td>
-                                <i class="material-symbols-outlined trash">delete_forever</i>
-                                <i class="material-symbols-outlined next">east</i>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div class="col ">
-                <h2>Cooking</h2>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Ticket</th>
-                            <th scope="col">Item</th>
-                            <th scope="col">Notes</th>
-                            <th scope="col">Edit</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Pizza</td>
-                            <td>Extra Cheese</td>
-                            <td>
-                                <i class="material-symbols-outlined trash">delete_forever</i>
-                                <i class="material-symbols-outlined next">east</i>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div class="col ">
-                <h2>Ready</h2>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Ticket</th>
-                            <th scope="col">Item</th>
-                            <th scope="col">Notes</th>
-                            <th scope="col">Edit</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Pizza</td>
-                            <td></td>
-                            <td>
-                                <i class="material-symbols-outlined trash">delete_forever</i>
-                                <i class="material-symbols-outlined next">east</i>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Burger</td>
-                            <td>Well Done</td>
-                            <td>
-                                <i class="material-symbols-outlined trash">delete_forever</i>
-                                <i class="material-symbols-outlined next">east</i>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+    <main className="container col-9">
+      <h1>Current Orders</h1>
+      <div className="row container">
+        <div className="col ">
+          <h2>New Orders</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Ticket</th>
+                <th scope="col">Item</th>
+                <th scope="col">Notes</th>
+                <th scope="col">Edit</th>
+              </tr>
+            </thead>
+            <tbody>
+              {order.map((order) => {
+                if (order.status.statusID === 1) {
+                  return (
+                    <tr key={order.orderID}>
+                      <th scope="row">{order.ticket.ticketID}</th>
+                      <td>{order.menu.menuItem}</td>
+                      <td>{order.menu.notes}</td>
+                      <td>
+                        <i className="material-symbols-outlined trash">
+                          delete_forever
+                        </i>
+                        <i className="material-symbols-outlined next">east</i>
+                      </td>
+                    </tr>
+                  );
+                }
+              })}
+            </tbody>
+          </table>
         </div>
+        <div className="col ">
+          <h2>Cooking</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Ticket</th>
+                <th scope="col">Item</th>
+                <th scope="col">Notes</th>
+                <th scope="col">Edit</th>
+              </tr>
+            </thead>
+            <tbody>
+              {order.map((order) => {
+                if (order.status.statusID === 2) {
+                  return (
+                    <tr key={order.orderID}>
+                      <th scope="row">{order.ticket.ticketID}</th>
+                      <td>{order.menu.menuItem}</td>
+                      <td>{order.menu.notes}</td>
+                      <td>
+                        <i className="material-symbols-outlined trash">
+                          delete_forever
+                        </i>
+                        <i className="material-symbols-outlined next">east</i>
+                      </td>
+                    </tr>
+                  );
+                }
+              })}
+            </tbody>
+          </table>
+        </div>
+        <div className="col ">
+          <h2>Ready</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Ticket</th>
+                <th scope="col">Item</th>
+                <th scope="col">Notes</th>
+                <th scope="col">Edit</th>
+              </tr>
+            </thead>
+            <tbody>
+              {order.map((order) => {
+                if (order.status.statusID === 3) {
+                  return (
+                    <tr key={order.orderID}>
+                      <th scope="row">{order.ticket.ticketID}</th>
+                      <td>{order.menu.menuItem}</td>
+                      <td>{order.menu.notes}</td>
+                      <td>
+                        <i className="material-symbols-outlined trash">
+                          delete_forever
+                        </i>
+                        <i className="material-symbols-outlined next">east</i>
+                      </td>
+                    </tr>
+                  );
+                }
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
