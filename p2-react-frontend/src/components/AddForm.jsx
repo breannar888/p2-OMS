17eb3001aaf94dd81de4f7c01319b4bcf8839711
breannar888 <67029835+breannar888@users.noreply.github.com>
2022-07-29 14:47:57 -0500

    add menu items to dropdown

--- p2-react-frontend/src/components/AddForm.jsx
+++ p2-react-frontend/src/components/AddForm.jsx
@@ -1,25 +1,25 @@
 import React from "react";
+import { OrderState } from "../context/OrderContext";
 
 export const AddForm = () => {
+  const { menu } = OrderState();
+
   return (
     <main className="container col-9 col-lg-10 p-3">
       <h1>Add Order</h1>
       <form>
         <div className="row row-cols-1">
           <div className="form-group col-11">
-            <label for="item">Menu Item</label>
+            <label htmlFor="item">Menu Item</label>
             <select className="form-select" id="item">
-              <option selected>&nbsp;</option>
-              <option value="1">Asiago Chicken Pasta</option>
-              <option value="2">Argentinian-Style Steak</option>
-              <option value="3">All-American Burger</option>
-              <option value="4">Shrimp Scampi</option>
-              <option value="5">Chicken Fajitas</option>
-              <option value="6">Grilled Salmon</option>
+              <option defaultValue="">&nbsp;</option>
+              {menu.map((item) => (
+                <option value={item.menuItem} key={item.menuID}>{item.menuItem}</option>
+              ))}
             </select>
           </div>
           <div className="form-group col-11">
-            <label for="notes">Notes</label>
+            <label htmlFor="notes">Notes</label>
             <textarea
               name="notes"
               className="form-control"
