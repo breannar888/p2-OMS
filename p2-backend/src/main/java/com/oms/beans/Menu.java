f16b3086feccef3decfb42975583a580e2921289
breannar888 <67029835+breannar888@users.noreply.github.com>
2022-07-29 12:45:10 -0500

    add orders get queries

--- p2-backend/src/main/java/com/oms/beans/Menu.java
+++ p2-backend/src/main/java/com/oms/beans/Menu.java
@@ -1,5 +1,7 @@
 package com.oms.beans;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,7 +9,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "Menu")
@@ -28,9 +33,9 @@ public class Menu {
 	@Column(name= "Image_Path")
 	private String imagePath;
 	
-	@ManyToOne // DO NOT use Eager unless you 100% ALWAYS need the child record
-	@JoinColumn(name = "Order_ID")
-	private Orders orders;
+	@OneToMany(mappedBy = "menu")
+	@JsonIgnore
+	private Set<Orders> orders;
 
 	public Menu() {
 		super();
@@ -67,19 +72,13 @@ public class Menu {
 	public void setImagePath(String imagePath) {
 		this.imagePath = imagePath;
 	}
-	
-	public Orders getOrders() {
+
+	public Set<Orders> getOrders() {
 		return orders;
 	}
 
-	public void setOrders(Orders orders) {
+	public void setOrders(Set<Orders> orders) {
 		this.orders = orders;
-	}
-
-	@Override
-	public String toString() {
-		return "Menu [menuID=" + menuID + ", menuItem=" + menuItem + ", price=" + price + ", imagePath=" + imagePath
-				+ "]";
 	}
 
 }
--- p2-backend/src/main/java/com/oms/beans/Status.java
+++ p2-backend/src/main/java/com/oms/beans/Status.java
@@ -1,32 +1,52 @@
 package com.oms.beans;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
-//@Entity
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "Status")
 public class Status {
 	
-	//@Id
-	//@GeneratedValue(strategy = GenerationType.IDENTITY)
-	//@Column
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "Status_ID")
 	private int statusID;
 	
-	//@Column
+	@Column(name = "Status_Code")
 	private String stausCode;
+	
+	@OneToMany(mappedBy = "status")
+	@JsonIgnore
+	private Set<Orders> orders;
 	
 	public Status() {
 		super();
 	}
 	
-	public Status(int statusID, String stausCode) {
+	public Status(int statusID, String stausCode, Set<Orders> orders) {
 		super();
 		this.statusID = statusID;
 		this.stausCode = stausCode;
+		this.orders = orders;
 	}
-	
+
+	public Set<Orders> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(Set<Orders> orders) {
+		this.orders = orders;
+	}
+
 	public int getStatusID() {
 		return statusID;
 	}
@@ -42,12 +62,5 @@ public class Status {
 	public void setStausCode(String stausCode) {
 		this.stausCode = stausCode;
 	}
-	
-	@Override
-	public String toString() {
-		return "Status [statusID=" + statusID + ", stausCode=" + stausCode + "]";
-	}
-	
-	
 	
 }
