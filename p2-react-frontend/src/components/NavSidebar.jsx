fa8e409523e3694ab6d244ef5ae163e76405ff22
breannar888 <67029835+breannar888@users.noreply.github.com>
2022-07-29 14:48:06 -0500

    fix spacing

--- p2-react-frontend/src/components/NavSidebar.jsx
+++ p2-react-frontend/src/components/NavSidebar.jsx
@@ -1,18 +1,16 @@
-import React from "react"
+import React from "react";
 
-export const NavSidebar = ({children}, location) => {
-    return (
-        <aside className="col-3 col-lg-2">  
-            <div className="header">Order <br /> Management <br /> System</div>
-            <ul className="nav d-block h-100">
-            
-                    {React.Children.map(children, child => (
-                        <li className="nav-item">
-                            {child}
-                        </li>
-                    ))}
-            
-            </ul>
-        </aside>
-    )
-}
+export const NavSidebar = ({ children }, location) => {
+  return (
+    <aside className="col-3 col-lg-2">
+      <div className="header">
+        Order <br /> Management <br /> System
+      </div>
+      <ul className="nav d-block h-100">
+        {React.Children.map(children, (child) => (
+          <li className="nav-item">{child}</li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
