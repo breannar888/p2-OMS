75efd53e10c47f84177cc361c9de955c25e90321
breannar888 <67029835+breannar888@users.noreply.github.com>
2022-08-01 13:58:55 -0500

    add ticket logic

--- p2-react-frontend/src/components/AddForm.jsx
+++ p2-react-frontend/src/components/AddForm.jsx
@@ -1,22 +1,46 @@
 import React from "react";
 import { OrderState } from "../context/OrderContext";
+import { TicketState } from "../context/OrderContext";
 
 export const AddForm = () => {
   const { menu } = OrderState();
+
+  const [ticketNew, setTicketNew] = useState(true);
+  const { ticket, setTicket } = TicketState();
+
+  const ticketChange = (event) => {
+    console.log("please");
+    event.target.value === "new" ? setTicketNew(true) : setTicketNew(false);
+    console.log(ticketNew);
+  }
 
   return (
     <main className="container col-9 col-lg-10 p-3">
       <h1>Add Order</h1>
       <form>
         <div className="row row-cols-1">
-          <div className="form-group col-11">
-            <label htmlFor="item">Menu Item</label>
-            <select className="form-select" id="item">
+          <div className="row row-cols-2">
+            <div className="form-group col-5">
+              <label htmlFor="item">Menu Item</label>
+              <select className="form-select" id="item">
               <option defaultValue="">&nbsp;</option>
               {menu.map((item) => (
                 <option value={item.menuItem} key={item.menuID}>{item.menuItem}</option>
               ))}
-            </select>
+            </select> 
+            </div>
+            <div className="col-1"></div>
+            <div className="form-group col-5">
+              <label htmlFor="item">Tickets</label>
+              <select className="form-select" id="item" onChange={ticketChange} >
+                <option value="new" >(Add ticket)</option>
+                {ticket.map((ticket) => {
+                  return (
+                    <option key={ticket.ticketID} value={ticket.ticketID}>{ticket.ticketName}</option>
+                  )
+                })}
+              </select>
+            </div>
           </div>
           {ticketNew ? <TicketName /> : <></> }
           <div className="form-group col-11">
