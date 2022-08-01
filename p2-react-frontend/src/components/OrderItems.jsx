e56444f0e0210cf1b6ae85fc628c08fdfd1eadc5
breannar888 <67029835+breannar888@users.noreply.github.com>
2022-08-01 11:03:02 -0500

    add order component

--- p2-react-frontend/src/components/OrderItems.jsx
+++ p2-react-frontend/src/components/OrderItems.jsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { OrderState } from "../context/OrderContext";
 import axios from "axios";
 
 export const OrderItems = ({ order }) => {
-  const { updateValues, setUpdateValues } = OrderState();
+  const { updateValues, setUpdateValues, menu } = OrderState();
+  const [toggle, setToggle] = useState(false);
 
   const updateStatus = async () => {
     try {
       axios
         .put(`http://localhost:8080/order/${order.orderID}`, {
           status: { statusID: order.status.statusID + 1 },
-          menu: { menuID: order.menu.menuID, },
-          ticket: { ticketID: order.ticket.ticketID, },
+          menu: { menuID: order.menu.menuID },
+          ticket: { ticketID: order.ticket.ticketID },
           notes: order.notes,
         })
         .then((res) => {
@@ -26,29 +27,58 @@ export const OrderItems = ({ order }) => {
 
   const handleDelete = async () => {
     try {
-      axios.delete(`http://localhost:8080/order/${order.orderID}`).then((res) => {
-        if (res.status === 204) {
-          setUpdateValues(!updateValues);
-        }
-      });
+      axios
+        .delete(`http://localhost:8080/order/${order.orderID}`)
+        .then((res) => {
+          if (res.status === 204) {
+            setUpdateValues(!updateValues);
+          }
+        });
     } catch (err) {
       console.log(err);
     }
   };
 
+  const handleUpdate = async () => {
+    setToggle(!toggle);
+  };
+
   return (
     <tr key={order.orderID}>
       <th scope="row">{order.ticket.ticketID}</th>
-      <td>{order.menu.menuItem}</td>
-      <td>{order.notes}</td>
+      {toggle ? (
+        <td>
+          {
+            <select name="cars" id="cars">
+              <option value={order.menu.menuItem}>{order.menu.menuItem}</option>
+              {menu.map((item) => {
+                return <option value={item.menuItem} key={item.menuID}>{item.menuItem}</option>;
+              })}
+            </select>
+          }
+        </td>
+      ) : (
+        <td>{order.menu.menuItem}</td>
+      )}
+      {toggle ? (
+        <td>
+          <input name="notes" />
+        </td>
+      ) : (
+        <td>{order.notes}</td>
+      )}
       <td>
         <i onClick={handleDelete} className="material-symbols-outlined trash">
           delete_forever
         </i>
         <i
-          onClick={updateStatus}
-          className="material-symbols-outlined next"
+          onClick={handleUpdate}
+          className="material-symbols-outlined edit"
+          title="Edit order"
         >
+          edit
+        </i>
+        <i onClick={updateStatus} className="material-symbols-outlined next">
           east
         </i>
       </td>
