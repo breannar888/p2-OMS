5ef86f267bda6ff38b676c085c7791f63aa1f057
froyo49 <kgarcia@skillstorm.com>
2022-08-02 18:35:08 -0400

    Update NavSidebar.jsx
    auto-formatting

--- p2-react-frontend/src/components/NavSidebar.jsx
+++ p2-react-frontend/src/components/NavSidebar.jsx
@@ -3,20 +3,21 @@ import { useLocation } from "react-router-dom";
 
 export const NavSidebar = ({ children }) => {
 
-    const {pathname} = useLocation();
-  if (pathname !== "/"){
-  return (
-    <aside className="col-3 col-lg-2">
-      <div className="header">
-        Order <br /> Management <br /> System
-      </div>
-      <ul className="nav d-block h-100">
-        {React.Children.map(children, (child) => (
-          <li className="nav-item">{child}</li>
-        ))}
-      </ul>
-    </aside>
-  );} else {
+  const { pathname } = useLocation();
+  if (pathname !== "/") {
+    return (
+      <aside className="col-3 col-lg-2">
+        <div className="header">
+          Order <br /> Management <br /> System
+        </div>
+        <ul className="nav d-block h-100">
+          {React.Children.map(children, (child) => (
+            <li className="nav-item">{child}</li>
+          ))}
+        </ul>
+      </aside>
+    );
+  } else {
     <></>
   }
 };
