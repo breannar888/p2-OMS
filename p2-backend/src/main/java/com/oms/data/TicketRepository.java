6c4bbb873bfd7834a3e2991dcf625ec0d3666134
froyo49 <kgarcia@skillstorm.com>
2022-08-08 17:43:34 -0400

    Update TicketRepository.java
    deleted one unnecessary line

--- p2-backend/src/main/java/com/oms/data/TicketRepository.java
+++ p2-backend/src/main/java/com/oms/data/TicketRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 
-import com.oms.beans.Sum;
 import com.oms.beans.Tickets;
 
 public interface TicketRepository extends JpaRepositoryImplementation<Tickets, Integer> {
