4915dcf2815010ec512662a5edfa1f5a4dc67474
breannar888 <67029835+breannar888@users.noreply.github.com>
2022-08-09 14:22:11 -0500

    add authorities into cookie

--- p2-react-frontend/src/components/NavSidebar.jsx
+++ p2-react-frontend/src/components/NavSidebar.jsx
@@ -13,8 +13,7 @@ export const NavSidebar = ({ children }) => {
       mode: "no-cors",
     }).then(() => {
       setCookie("JSESSIONID", undefined);
-      removeCookie("JSESSION");
-      removeCookie("[object Object]");
+      removeCookie("Authorities");
       navigate("/");
     });
   };
