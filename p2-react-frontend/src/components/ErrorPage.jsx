d1493dae01fdebc86f25a31cf1b22736aa9d486c
breannar888 <67029835+breannar888@users.noreply.github.com>
2022-08-09 12:08:52 -0500

    error page component

---
+++ p2-react-frontend/src/components/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const ErrorPage = () => {
+  return (
+    <main className="container col-9 col-lg-10 p-3">
+      <div className="container">
+        <h2>Page not found</h2>
+      </div>
+    </main>
+  );
+};
