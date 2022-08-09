c56ba0fd77545269a3875d4c2e89133cce3a659a
breannar888 <67029835+breannar888@users.noreply.github.com>
2022-08-09 12:10:31 -0500

    login functionality

--- p2-react-frontend/src/components/NavSidebar.jsx
+++ p2-react-frontend/src/components/NavSidebar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { setCookie } from "react-use-cookie";
 
 export const NavSidebar = ({ children }) => {
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    await fetch("http://localhost:8080/logout", {
+      mode: "no-cors",
+    }).then(() => {
+      setCookie("JSESSIONID", undefined);
+      navigate("/");
+    });
+  };
 
   const { pathname } = useLocation();
   if (pathname !== "/") {
@@ -14,10 +26,11 @@ export const NavSidebar = ({ children }) => {
           {React.Children.map(children, (child) => (
             <li className="nav-item">{child}</li>
           ))}
+          <button onClick={logout}>Logout</button>
         </ul>
       </aside>
     );
   } else {
-    <></>
+    <></>;
   }
 };
