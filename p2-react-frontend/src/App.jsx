d96876258696acfac58212211dacd72301889f71
breannar888 <67029835+breannar888@users.noreply.github.com>
2022-08-09 12:08:39 -0500

    error page routing, add cookie provider

--- p2-react-frontend/src/App.jsx
+++ p2-react-frontend/src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
 import "./App.css";
 import {
   Login,
@@ -7,33 +12,47 @@ import {
   OrdersLog,
   OrdersTable,
   Menu,
+  ErrorPage,
 } from "./components/";
 import OrderProvider from "./context/OrderContext";
+import { CookiesProvider } from "react-cookie";
 
 function App() {
 
+  //add in permissions based on authorities 
+  //only manager can view Manage Menu link
+  
   return (
     <div className="row">
-      <OrderProvider>
-        <BrowserRouter>
-          <NavSidebar>
-            <NavLink to="/add" className="nav-link link-dark"> Add Orders</NavLink>
-            <NavLink to="/current" className="nav-link link-dark"> Current Orders</NavLink>
-            <NavLink to="/menu" className="nav-link link-dark"> Manage Menu</NavLink>
-            <NavLink to="/log" className="nav-link link-dark"> Orders Log</NavLink>
-          </NavSidebar>
+      <CookiesProvider>
+        <OrderProvider>
+          <BrowserRouter>
+            <NavSidebar>
+              <NavLink to="/add" className="nav-link link-dark">
+                Add Orders
+              </NavLink>
+              <NavLink to="/current" className="nav-link link-dark">
+                Current Orders
+              </NavLink>
+              <NavLink to="/menu" className="nav-link link-dark">
+                Manage Menu
+              </NavLink>
+              <NavLink to="/log" className="nav-link link-dark">
+                Orders Log
+              </NavLink>
+            </NavSidebar>
 
-
-          <Routes>
-            <Route path="/" element={<Login />}></Route>
-            <Route path="/add" element={<AddForm />}></Route>
-            <Route path="/current" element={<OrdersTable />}></Route>
-            <Route path="/menu" element={<Menu />}></Route>
-            <Route path="/log" element={<OrdersLog />}></Route>
-            <Route path="/*" element={<AddForm />}></Route>
-          </Routes>
-        </BrowserRouter>
-      </OrderProvider>
+            <Routes>
+              <Route path="/" element={<Login />}></Route>
+              <Route path="/add" element={<AddForm />}></Route>
+              <Route path="/current" element={<OrdersTable />}></Route>
+              <Route path="/menu" element={<Menu />}></Route>
+              <Route path="/log" element={<OrdersLog />}></Route>
+              <Route path="/*" element={<ErrorPage />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </OrderProvider>
+      </CookiesProvider>
     </div>
   );
 }
